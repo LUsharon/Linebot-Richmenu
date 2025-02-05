d88445d503d7012a4d3469c377d8c37320916de2
LUsharon <112764801+LUsharon@users.noreply.github.com>
2025-02-05 23:19:01 +0800

    Update app.py
    修正程式錯誤，並加入文字echo

--- app.py
+++ app.py
@@ -9,18 +9,32 @@ from linebot.v3.messaging import (
     Configuration,
     ApiClient,
     MessagingApi,
+    ReplyMessageRequest,
     MessagingApiBlob,
     RichMenuSize,
     RichMenuRequest,
     RichMenuArea,
     RichMenuBounds,
-    MessageAction
+    MessageAction,
+    TextMessage,
+    Emoji,
+    VideoMessage,
+    AudioMessage,
+    LocationMessage,
+    StickerMessage,
+    ImageMessage
+)
+
+from linebot.v3.webhooks import (
+    MessageEvent,
+    TextMessageContent,
 )
 import requests
 import json
 import os
 
 app = Flask(__name__)
+
 
 channel_access_tokenn = os.getenv('CHANNEL_ACCESS_TOKEN')
 line_handler = WebhookHandler(os.getenv('CHANNEL_SECRET'))
@@ -131,117 +145,91 @@ def create_rich_menu_1():
 
         line_bot_api.set_default_rich_menu(rich_menu_id)
 
-
-def create_rich_menu_2():
+@line_handler.add(MessageEvent, message=TextMessageContent)
+def handle_message(event):
+    text = event.message.text
     with ApiClient(configuration) as api_client:
         line_bot_api = MessagingApi(api_client)
-        line_bot_blob_api = MessagingApiBlob(api_client)
 
-        # Create rich menu
-        headers = {
-            'Authorization': 'Bearer ' + CHANNEL_ACCESS_TOKEN,#授權
-            'Content-Type': 'application/json'
-        }
-        body = {#true false首字變大寫
-            "size": {
-                "width": 2500,
-                "height": 1686
-            },
-            "selected": True,
-            "name": "圖文選單 1",
-            "chatBarText": "查看更多資訊",
-            "areas": [
-                {
-                    "bounds": {
-                        "x": 0,
-                        "y": 0,
-                        "width": 833,
-                        "height": 843
-                    },
-                    "action": {
-                        "type": "message",
-                        "text": "A"
-                    }
-                },
-                {
-                    "bounds": {
-                        "x": 834,
-                        "y": 0,
-                        "width": 833,
-                        "height": 843
-                    },
-                    "action": {
-                        "type": "message",
-                        "text": "B"
-                    }
-                },
-                {
-                    "bounds": {
-                        "x": 1663,
-                        "y": 0,
-                        "width": 834,
-                        "height": 843
-                    },
-                    "action": {
-                        "type": "message",
-                        "text": "C"
-                    }
-                },
-                {
-                    "bounds": {
-                        "x": 0,
-                        "y": 843,
-                        "width": 833,
-                        "height": 843
-                    },
-                    "action": {
-                        "type": "message",
-                        "text": "D"
-                    }
-                },
-                {
-                    "bounds": {
-                        "x": 834,
-                        "y": 843,
-                        "width": 833,
-                        "height": 843
-                    },
-                    "action": {
-                        "type": "message",
-                        "text": "E"
-                    }
-                },
-                {
-                    "bounds": {
-                        "x": 1662,
-                        "y": 843,
-                        "width": 838,
-                        "height": 843
-                    },
-                    "action": {
-                        "type": "message",
-                        "text": "F"
-                    }
-                }
+        if text == '文字':
+            line_bot_api.reply_message(
+                ReplyMessageRequest(
+                    reply_token=event.reply_token,
+                    messages=[TextMessage(text="這是文字訊息")]
+                )
+            )
+        elif text == '表情符號':
+            emojis = [
+                Emoji(index=0, product_id="5ac1bfd5040ab15980c9b435", emoji_id="001"),
+                Emoji(index=12, product_id="5ac1bfd5040ab15980c9b435", emoji_id="002")
             ]
-        }
-
-        response = requests.post('https://api.line.me/v2/bot/richmenu', headers=headers, data=json.dumps(body).encode('utf-8'))
-        response = response.json()
-        print(response)
-        rich_menu_id = response["richMenuId"]
-        
-        # Upload rich menu image
-        with open('static/richmenu-1.jpg', 'rb') as image:
-            line_bot_blob_api.set_rich_menu_image(
-                rich_menu_id=rich_menu_id,
-                body=bytearray(image.read()),
-                _headers={'Content-Type': 'image/jpeg'}
+            line_bot_api.reply_message(
+                ReplyMessageRequest(
+                    reply_token=event.reply_token,#$字號透過前面的索引
+                    messages=[TextMessage(text='$ LINE 表情符號 $', emojis=emojis)]
+                )
+            )
+        elif text == '貼圖':
+            line_bot_api.reply_message(
+                ReplyMessageRequest(
+                    reply_token=event.reply_token,
+                    messages=[StickerMessage(package_id="446", sticker_id="1988")]
+                )
+            )
+        elif text == '圖片':
+            url = request.url_root + 'static/Logo.jpg'
+            url = url.replace("http", "https")
+            app.logger.info("url=" + url)
+            line_bot_api.reply_message(
+                ReplyMessageRequest(
+                    reply_token=event.reply_token,
+                    messages=[
+                        ImageMessage(original_content_url=url, preview_image_url=url)
+                    ]
+                )
+            )
+        elif text == '影片':
+            url = request.url_root + 'static/video.mp4'
+            url = url.replace("http", "https")
+            app.logger.info("url=" + url)
+            line_bot_api.reply_message(
+                ReplyMessageRequest(
+                    reply_token=event.reply_token,
+                    messages=[
+                        VideoMessage(original_content_url=url, preview_image_url=url)
+                    ]
+                )
+            )
+        elif text == '音訊':
+            url = request.url_root + 'static/music.mp3'
+            url = url.replace("http", "https")
+            app.logger.info("url=" + url)
+            duration = 60000  # in milliseconds毫秒為單位
+            line_bot_api.reply_message(
+                ReplyMessageRequest(
+                    reply_token=event.reply_token,
+                    messages=[
+                        AudioMessage(original_content_url=url, duration=duration)#音訊路徑、時長
+                    ]
+                )
+            )
+        elif text == '位置':
+            line_bot_api.reply_message(
+                ReplyMessageRequest(
+                    reply_token=event.reply_token,
+                    messages=[
+                        LocationMessage(title='Location', address="Taipei", latitude=25.0475, longitude=121.5173)
+                    ]
+                )
+            )
+        else:
+            line_bot_api.reply_message(
+                ReplyMessageRequest(
+                    reply_token=event.reply_token,
+                    messages=[TextMessage(text=event.message.text)]
+                )
             )
 
-        line_bot_api.set_default_rich_menu(rich_menu_id)
-
-create_rich_menu_2()
 
 if __name__ == "__main__":
     app.run()
